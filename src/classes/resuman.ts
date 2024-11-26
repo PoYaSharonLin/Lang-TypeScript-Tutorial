8640b857fdafc0de887cc9920e14a7629e794260
sharon.lin <sharon.lin@iss.nthu.edu.tw>
2024-11-26 20:45:52 +0800

    add: add export

--- src/classes/resuman.ts
+++ src/classes/resuman.ts
@@ -1,4 +1,4 @@
-class resuman {
+export class resuman {
   constructor(private title: string, private section_counts: number){}
   public format() {
     return `${this.title} consists of ${this.section_counts} sections`;
